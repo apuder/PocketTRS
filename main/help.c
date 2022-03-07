10dbaca7258de913c02bfff3ba9d138ad181c721
Arno Puder <arno@puder.org>
2022-03-06 16:41:03 -0800

    Update help screen with new key mappings

--- main/help.c
+++ main/help.c
@@ -9,12 +9,12 @@ void help()
   init_window(&wnd, 0, 3, 0, 0);
   header("Help");
   wnd_print(&wnd, false, "\nKey mapping:\n");
-  wnd_print(&wnd, false, "  <F1>  - Enable lowercase\n");
-  wnd_print(&wnd, false, "  <F2>  - <CLEAR>\n");
-  wnd_print(&wnd, false, "  <F3>  - PocketTRS Configuration\n");
-  wnd_print(&wnd, false, "  <F4>  - Send screenshot to the printer\n");
+  wnd_print(&wnd, false, "  <F4>  - Enable lowercase\n");
+  wnd_print(&wnd, false, "  <F5>  - PocketTRS Configuration\n");
+  wnd_print(&wnd, false, "  <F6>  - Send screenshot to the printer\n");
   wnd_print(&wnd, false, "  <F9>  - Reset Z80\n");
   wnd_print(&wnd, false, "  <ESC> - <BREAK>\n");
+  wnd_print(&wnd, false, "  \\    - <CLEAR>\n");
   wnd_print(&wnd, false, "\nVisit https://github.com/apuder/PocketTRS for "
             "complete online documentation.");
   screen_show(false);
